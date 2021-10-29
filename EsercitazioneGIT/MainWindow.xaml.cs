3ccf45fce54316f5dbece652134e2eece9b0121f
Giacomo Vignali <GIACOMO.VIGNALI@local.ittcesena.it>
2021-10-29 09:42:05 +0200

    Interfaccia brani e cd

--- EsercitazioneGIT/MainWindow.xaml.cs
+++ EsercitazioneGIT/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace EsercitazioneGIT
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Cd> _listCD;
+        private Autore autore;
+        private Cd cd;
+        
         public MainWindow()
         {
+
             InitializeComponent();
+            txt_cd.Text = "NUMERO BRANI CD: 0" + '\n' + "DURATA CD: 0 secondi";
+
+
+           
+
+        }
+
+        private void btn_aggiungiBrano_Click(object sender, RoutedEventArgs e)
+        {
+            string titolo = txt_nomeCanzone.Text;
+            string nomeAutore = txt_nomeAutore.Text;
+            double durata = double.Parse(txt_durataCanzone.Text);
+
+            autore = new autore(titolo, nomeAutore, durata);
+
+            lst_listaCanzoni.Items.Add(autore);
+        }
+
+        private void btn_creaCd_Click(object sender, RoutedEventArgs e)
+        {
+            string titolo = txt_nomeCanzone.Text;
+            string autore = txt_nomeAutore.Text;
+
+            cd = new Cd(titolo, autore);
+            if(lst_listaCanzoni.SelectedItem != null)
+            {
+                lst_CD.Items.Add(cd._listBrani);
+            }
+            
+
         }
     }
+
 }
